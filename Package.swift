cdbcb9972c49e3653c5b3b1d75c68030b68ebf2c
Stefan Wieland <stefan.wieland@allaboutapps.at>
2020-10-12 11:32:39 +0200

    update binaryTarget url

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OPPWAMobile",
-            url: "https://github.com/wieweb/OPPWAMobile/releases/download/0.2/OPPWAMobile.xcframework.zip",
+            url: "https://github.com/wieweb/OPPWAMobile/releases/download/0.1.0/OPPWAMobile.xcframework.zip",
             checksum: "712f8f5444625aeb0358dfe9bdc97114e62ad247cdbcc7ccefde389ea0b24a9b"
         )
     ]
