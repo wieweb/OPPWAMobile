573589852bf61573090170eab433cf023209df68
Stefan Wieland <stefan.wieland@allaboutapps.at>
2020-10-12 09:52:34 +0200

    change binary url

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OPPWAMobile",
-            url: "https://drive.google.com/file/d/1q3TGsKVaFZITSWYxWs5Fn-Dy4cecDBwu/view?usp=sharing",
+            url: "https://github.com/wieweb/OPPWAMobile/releases/download/0.1/OPPWAMobile.xcframework.zip",
             checksum: "d38d9fd5b3b59bf8bd84ae281078fd791301cf56457d29be911d869eed72074d"
         )
     ]
